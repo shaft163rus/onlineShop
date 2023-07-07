eb9fe3e69da299744626994ca56d1b92d9724078
shaft163 <shaft163rus@gmail.com>
2023-07-07 06:27:31 +0400

    Не получается со scope, куда не пробовал не работает как надо.

---
+++ src/main/java/com/skypro/onlineshop/services/ProductServiceImpl.java
@@ -0,0 +1,35 @@
+package com.skypro.onlineshop.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skypro.onlineshop.model.Product;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+@Service
+
+public class ProductServiceImpl implements ProductService {
+
+    private final ArrayList<Product> basket = new ArrayList<>();
+
+    @Override
+    public String getAllProducts() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.writeValueAsString(basket);
+    }
+
+    @Override
+    public String addProduct(Integer... productId) {
+        for (Integer id : productId) {
+            Product product = new Product(id);
+            basket.add(product);
+        }
+
+        return basket.stream().map(Product::toString).collect(Collectors.joining(" | "));
+    }
+}
